59033268093aa7ec2acea8957d132fb7b946a7cc
Jr <122588832+gfcfctfvgvyg@users.noreply.github.com>
2026-02-27 07:31:16 -0600

    Delete validate.py

--- validate.py
+++
@@ -1,8 +0,0 @@
-import ast
-with open('main.py', 'r') as f:
-    ast.parse(f.read())
-print('main.py - OK')
-with open('config.py', 'r') as f:
-    ast.parse(f.read())
-print('config.py - OK')
-print('All valid!')
